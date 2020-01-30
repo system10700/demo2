b06781d73bf175e960f3f26bfa73febcd41fc05e
weh <weh@qq.com>
2020-01-30 17:39:16 +0800

    第一次修改项目

--- demo2/src/main/java/com/china/test/TestDemo.java
+++ demo2/src/main/java/com/china/test/TestDemo.java
@@ -5,4 +5,8 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("添加");
     }
+
+    public void show(){
+        System.out.println("第一次修改");
+    }
 }
